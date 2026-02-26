e82d263a124010734c4480c896ef0c8286822cc2
funktasthic <ignacioavendanoramirez@gmail.com>
2026-02-26 13:03:04 -0300

    docs: add mail service documentation

--- src/mail_service.py
+++ src/mail_service.py
@@ -5,16 +5,28 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from src.config import settings
 
 class MailService:
+    """
+    Handles email body rendering using templates.
+    """   
     def __init__(self, template_dir: str = 'templates'):
+        # Setup the template loader to look into templates folder
         self.jinja_env = Environment(
             loader=FileSystemLoader(template_dir),
             autoescape=select_autoescape(['html', 'xml'])
         )
-
+    """
+    Takes an HTML template
+    Returns the final HTML string.
+    """
     def render_body(self, template_name: str, **context) -> str:
+
         template = self.jinja_env.get_template(template_name)
         return template.render(**context)
 
+    """
+    Injects the environment variables
+    
+    """
     def send_email(self, recipient_email: str, subject: str, html_body: str):
         message = MIMEMultipart()
         
