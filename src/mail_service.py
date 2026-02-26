2bba778123c3d5da320bcea33623d097f0908aab
funktasthic <ignacioavendanoramirez@gmail.com>
2026-02-26 12:58:09 -0300

    feat: implement smtp delivery method with tls security

--- src/mail_service.py
+++ src/mail_service.py
@@ -1,23 +1,30 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from src.config import settings
 
 class MailService:
-    """
-    Handles email body rendering using templates.
-    """
     def __init__(self, template_dir: str = 'templates'):
-        # Setup the template loader to look into templates folder
         self.jinja_env = Environment(
             loader=FileSystemLoader(template_dir),
             autoescape=select_autoescape(['html', 'xml'])
         )
 
     def render_body(self, template_name: str, **context) -> str:
-        """
-        Takes an HTML template and injects variables
-        Returns the final HTML string.
-        """
-        try:
-            template = self.jinja_env.get_template(template_name)
-            return template.render(**context)
-        except Exception as e:
-            raise RuntimeError(f"Template rendering failed: {e}")
+        template = self.jinja_env.get_template(template_name)
+        return template.render(**context)
+
+    def send_email(self, recipient_email: str, subject: str, html_body: str):
+        message = MIMEMultipart()
+        
+        # Use the enviroment variables
+        message['From'] = settings.email_user 
+        message['To'] = recipient_email
+        message['Subject'] = subject
+        message.attach(MIMEText(html_body, 'html'))
+
+        with smtplib.SMTP(settings.smtp_server, settings.smtp_port) as server:
+            server.starttls()
+            server.login(settings.email_user, settings.email_pass)
+            server.send_message(message)
